a9ee7a0555594d4137f1e3b71d6c110fde39393a
lyl <luyilong1314@126.com>
2025-08-20 11:22:14 +0800

    [144] 二叉树的前序遍历

---
+++ cpp-template/leetcode/editor/mine/144.二叉树的前序遍历.cpp
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=cpp
+ * @lcpr version=30202
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include<stack>
+#include "../common/ListNode.cpp"
+#include "../common/TreeNode.cpp"
+#include "../common/utils.cpp"
+
+using namespace std;
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> s;
+        TreeNode* node = root;
+        while (node!=nullptr || !s.empty())
+        {
+            while(node!=nullptr)
+            {
+                res.push_back(node->val);
+                s.push(node->right);
+                node = node->left;       
+            }
+            node = s.top();
+            s.pop();
+        }
+        
+        return res;
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+    TreeNode* root = nullptr;
+    root = TreeNode::createRoot({1,std::nullopt,2,3});
+    vector<int> res = solution.preorderTraversal(root);
+    print_vec<int>(res);
+    TreeNode::freeTree(root);
+}
+
+
+
+/*
+// @lcpr case=start
+// [1,null,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,5,null,8,null,null,6,7,9]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
